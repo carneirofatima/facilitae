8d50f035985c866c884f7b3250c9c2502b2a6f6c
Fatima Carneiro <fatima.carneiro@wavy.global>
2021-03-09 23:04:14 -0300

    Adição do gitignore

---
+++ facilitae/src/main/kotlin/com/carneirofatima/facilitae/FacilitaeApplication.kt
@@ -0,0 +1,11 @@
+package com.carneirofatima.facilitae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FacilitaeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FacilitaeApplication>(*args)
+}
---
+++ facilitae/src/test/kotlin/com/carneirofatima/facilitae/FacilitaeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.carneirofatima.facilitae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FacilitaeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
